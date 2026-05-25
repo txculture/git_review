07f3d79dc4f03c73c5b167932ba587bacede8651
kangyi <297523949@qq.com>
2026-05-26 00:05:09 +0800

    DAY22,小项目：通讯录查询训练

--- python_practice.py
+++ python_practice.py
@@ -4,3 +4,14 @@ he = 0
 for num in range(1,101):
     he = he + num
 print(he)
+
+# DAY 22小项目：通讯录查询
+contacts = {"ming":"138001380000","gang":"13700137000"}
+
+def search(name):
+    if name in contacts:
+        print(name,"的电话是:",contacts[name])
+    else:
+        print("查无此人")
+
+search("输入通讯录名字查询")
