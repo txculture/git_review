43ade3bd6269c628b23f524b3e95ded4f30fd1c1
kangyi <297523949@qq.com>
2026-06-14 23:18:07 +0800

    项目：网站状态检查器

--- git_bdve.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def build_save():
-    pyname = input("请输入文件名（不加后缀）：")
-    os.system(f'echo. > {pyname}.py')
-    os.system(f'notepad {pyname}.py')
-    print(f'{pyname}.py 已创建并打开')
-
-build_save()
