0ea0be2c156c720a1815bf453e68d88c1631f7c9
kangyi <297523949@qq.com>
2026-06-19 20:15:10 +0800

    自动push项目更新，网页训练更新

--- git_save.py
+++ git_save.py
@@ -1,9 +1,9 @@
 import os
 
 def git_save():
-    filename = input("请输入要保存的文件名（不加后缀）：")
+    filename = input("请输入要保存的文件名（加后缀）：")
     message = input("请输入提交信息：")
-    os.system(f"git add {filename}.py")
+    os.system(f"git add {filename}")
     os.system(f'git commit -m "{message}"')
     os.system("git push")
     print(f" {filename}.py 已提交并推送到 GitHub")
